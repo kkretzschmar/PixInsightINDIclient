d721c641b779fbac9cf4027dc0c43c44eeef7a3e
Klaus Kretzschmar <kkretzschmar@gmail.com>
2015-09-05 16:49:23 +0200

    Fix gmock/gtest travis build

--- run_build.sh
+++ run_build.sh
@@ -16,10 +16,10 @@ then
  unzip gmock-1.7.0.zip
  echo "done"
  echo Building gtest ..."
- mkdir build && cd build && cmake ${GTEST_DIR} 
+ mkdir build && cd build && cmake ../${GTEST_DIR} && cd .. 
  echo "done"
  echo Building gmock ..."
- mkdir build && cd build && cmake ${GMOCK_DIR} 
+ mkdir build && cd build && cmake ../${GMOCK_DIR} && cd ..
  echo "done"
  
 elif [ "$TRAVIS_OS_NAME" = "osx" ];
