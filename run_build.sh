d3e39a43a681a170258f6d7c68beee5693bcc4eb
Klaus Kretzschmar <kkretzschmar@gmail.com>
2015-09-05 19:10:21 +0200

    Trigger gtests

--- run_build.sh
+++ run_build.sh
@@ -10,16 +10,21 @@ then
  # Build gmock and gtest
  export GTEST_DIR=gtest-1.7.0
  export GMOCK_DIR=gmock-1.7.0
- 
+ export GTEST_INC=${GTEST_DIR}/include
+ export GMOCK_INC=${GMOCK_DIR}/include
+ export GTEST_LIB=${GTEST_LIB}/lib
+ export GMOCK_LIB=${GMOCK_LIB}/lib
+ mkdir -p ${GTEST_LIB}/x64
+ mkdir -p ${GMOCK_LIB}/x64
  echo "Starting:  unzip gtest and gmock ..."
  unzip gtest-1.7.0.zip
  unzip gmock-1.7.0.zip
  echo "done"
  echo Building gtest ..."
- mkdir build && cd build && cmake ../${GTEST_DIR} && make && cd .. 
+ mkdir build && cd build && cmake ../${GTEST_DIR} && make && cp libgtest.a ${GTEST_LIB}/x64 && cp libgtest_main.a ${GTEST_LIB}/x64 && cd .. 
  echo "done"
  echo Building gmock ..."
- mkdir build && cd build && cmake ../${GMOCK_DIR} && make && cd ..
+ mkdir build && cd build && cmake ../${GMOCK_DIR} && make && cp libgmock.a ${GMOCK_LIB}/x64 && cd ..
  echo "done"
  
 elif [ "$TRAVIS_OS_NAME" = "osx" ];
@@ -35,4 +40,11 @@ echo $OS_PATH
 export PCLLIBDIR64=../../../PCL/lib/$OS_PATH/x64
 echo $PCLLIBDIR64
 pwd
-cd module/$OS_PATH/g++/ && mkdir -p x64/Release  && make -f makefile-x64 
+cd module/$OS_PATH/g++/ && mkdir -p x64/Release  && make -f makefile-x64  && make -f makefile-x64-debug-static && cd ../../../
+
+# build and run PixInsightINDIclient tests
+if [ "$TRAVIS_OS_NAME" = "linux" ]; 
+ cd test && make -f makefile-x64-debug && cd ..
+ # run tests
+ ${PCLBINDIR64}/PixInsightINDIclientTest
+fi
